19b4b2e0d1daa4f6b4493f3f2f2182b53fe01b82
v1ctor <victorviniciuspj@gmail.com>
2023-12-13 23:52:31 -0300

    feat(Navio): add buscar navio

--- js/controller/navioController.js
+++ js/controller/navioController.js
@@ -7,3 +7,11 @@ export async function getNavio(id) {
   const response = await fetch(`http://localhost:8081/api/navios/${id}`);
   return response.json();
 }
+
+export async function buscarNavio(nome, bandeira, codNavio) {
+  const response = await fetch(
+    `http://localhost:8081/api/navios/findByFiltro?nome=${nome}&bandeira=${bandeira}&codNavio=${codNavio}`
+    // `http://localhost:8081/api/navios/findByFiltro/Navio/brasil/123`
+  );
+  return response.json();
+}
--- js/service/navioService.js
+++ js/service/navioService.js
@@ -1,3 +1,9 @@
+import { buscarNavio, getNavios } from "../controller/navioController.js";
+
 export function cadastrarPedido(){
   console.log("Funcionalidade ainda não implementada");
+}
+
+export async function searchNavios(nome, bandeira, codNavio, container){
+  return await buscarNavio(nome, bandeira, codNavio);
 }
